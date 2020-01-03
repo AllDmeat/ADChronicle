15cee6494e7c01da1e1509140ea476c531f0392d
Aleksey Berezka <a.berezka@dodopizza.com>
2020-01-03 16:10:51 +0300

    Introduced ADLogger

--- Sources/ADChronicle/ADChronicle.swift
+++ Sources/ADChronicle/ADChronicle.swift
@@ -1,15 +1,17 @@
 import Foundation
 import os
 
+typealias ADLogger = OSLog
+
 @available(OSX 10.14, *)
 @available(iOS 12.0, *)
 public class ADChronicle {
-    public static func logger<Subject>(subsystem: String, category: Subject) -> OSLog {
+    public static func logger<Subject>(subsystem: String, category: Subject) -> ADLogger {
         return OSLog(subsystem: subsystem, category: category)
     }
     
     public static func log(_ error: Error,
-                           logger: OSLog,
+                           logger: ADLogger,
                            function: String = #function) {
         let message = String(reflecting: error)
         self.log(message, logger: logger, function: function, logType: .error)
@@ -17,7 +19,7 @@ public class ADChronicle {
     
     public static func log(_ message: String,
                            userInfo: CustomDebugStringConvertible? = nil,
-                           logger: OSLog,
+                           logger: ADLogger,
                            function: String = #function,
                            logType: OSLogType = .info) {
         let message = message + "\n\n" + "[User Info]:" + "\n" + String(describing: userInfo ?? [:])
